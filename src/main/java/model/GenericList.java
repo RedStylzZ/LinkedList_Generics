319b8c9f103542fea1204291ac41554b2df36259
Tizian <tizianappler@yahoo.de>
2021-12-16 11:35:02 +0100

    Changed to Type to Generic

--- src/main/java/model/GenericList.java
+++ src/main/java/model/GenericList.java
@@ -1,4 +1,55 @@
 package model;
 
-public class GenericList {
+public class GenericList<T extends Generic> {
+
+    private GenericListItem<T> head;
+
+    public GenericList(T generic) {
+        this.head = new GenericListItem<>(generic);
+    }
+
+    public void add(T generic) {
+        if (head == null) {
+            this.head = new GenericListItem<>(generic);
+            return;
+        }
+
+        GenericListItem<T> nextGenericList = head;
+
+        while (nextGenericList.getNext() != null) {
+            nextGenericList = nextGenericList.getNext();
+        }
+        nextGenericList.setNext(new GenericListItem<>(generic));
+    }
+
+    public void remove(T generic) {
+        if (head == null) return;
+        GenericListItem<T> nextGenericList = head;
+
+        while (nextGenericList.getNext() != null) {
+            if (head.equals(nextGenericList)) {
+                head = nextGenericList.getNext();
+                nextGenericList = nextGenericList.getNext();
+            }
+            if (nextGenericList.getNext().getValue().equals(generic)) {
+                nextGenericList.setNext(nextGenericList.getNext().getNext());
+                continue;
+            }
+            nextGenericList = nextGenericList.getNext();
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (head == null) return "";
+        StringBuilder stringBuilder = new StringBuilder();
+        GenericListItem<T> nextAnimalList = head;
+
+        while (nextAnimalList != null) {
+            stringBuilder.append(nextAnimalList.getValue().getName()).append(nextAnimalList.getNext() == null ? "" : " -> ");
+            nextAnimalList = nextAnimalList.getNext();
+        }
+
+        return stringBuilder.toString();
+    }
 }
--- src/main/java/model/GenericListItem.java
+++ src/main/java/model/GenericListItem.java
@@ -1,5 +1,39 @@
 package model;
 
-public class GenericListItem {
+import java.util.Objects;
 
+public class GenericListItem<T extends Generic> {
+
+    private final T value;
+    private GenericListItem<T> next;
+
+    public GenericListItem(T value) {
+        this.value = value;
+    }
+
+    public void setNext(GenericListItem<T> next) {
+        this.next = next;
+    }
+
+    public T getValue() {
+        return this.value;
+    }
+
+    public GenericListItem<T> getNext() {
+        return this.next;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GenericListItem<?> that = (GenericListItem<?>) o;
+        return Objects.equals(value, that.value) && Objects.equals(next, that.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, next);
+    }
 }
